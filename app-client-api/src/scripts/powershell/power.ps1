d1276383183a6ffc1a91a5f8350b9ed6ed96ff84
odiegosouza <diego.santos@sptech.school>
2024-04-23 11:06:41 -0300

    adicionando captura de temperatura de bateria de linux e windows

--- app-client-api/src/scripts/powershell/power.ps1
+++ app-client-api/src/scripts/powershell/power.ps1
@@ -1,1 +1,21 @@
-Get-WmiObject -Class Win32_Battery | Select-Object EstimatedChargeRemaining, EstimatedRunTime
+if ($env:OS -like "*Windows_NT*") {
+    $batteryInfo = Get-WmiObject -Class Win32_Battery | Select-Object EstimatedChargeRemaining
+    $batteryPercentage = $batteryInfo.EstimatedChargeRemaining
+    Write-Output "Porcentagem da bateria (Windows): $batteryPercentage%"
+} else {
+    $result = upower -i /org/freedesktop/UPower/devices/battery_BAT0
+
+    if ($?) {
+        $lines = $result -split "\n"
+
+        foreach ($line in $lines) {
+            if ($line -like '*percentage:*') {
+                $batteryPercentage = ($line -split ':')[1].Trim()
+                Write-Output "Porcentagem da bateria (Linux): $batteryPercentage"
+                break
+            }
+        }
+    } else {
+        Write-Output "O comando 'upower' não está disponível. Verifique se está instalado."
+    }
+}
