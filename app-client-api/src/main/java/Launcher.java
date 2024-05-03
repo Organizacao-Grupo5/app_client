509e5776b3db43f009f1e8fc1d15c84ce266d934
ClaudioAraujo03 <claudio.araujofo@sptech.school>
2024-05-03 18:08:20 -0300

    Tornando o jar clicavel

---
+++ app-client-api/src/main/java/Launcher.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+
+public class Launcher {
+    public static void main(String[] args) {
+        try {
+            String command = "powershell.exe -Command \"Start-Process java -ArgumentList ('-jar', 'app-client-api-1.0-SNAPSHOT-jar-with-dependencies.jar') -Verb RunAs\"";
+            Process process = Runtime.getRuntime().exec(command);
+            process.waitFor();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
