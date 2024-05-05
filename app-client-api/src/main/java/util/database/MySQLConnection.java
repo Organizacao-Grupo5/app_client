0f8828897704a2dba0dd2724d152953a6f0a52a0
ClaudioAraujo03 <claudio.araujofo@sptech.school>
2024-05-05 13:26:18 -0300

    Configuração da ec2

--- app-client-api/src/main/java/util/database/MySQLConnection.java
+++ app-client-api/src/main/java/util/database/MySQLConnection.java
@@ -9,9 +9,19 @@ import java.util.logging.Logger;
 public class MySQLConnection {
 	private static final Logger logger = Logger.getLogger(MySQLConnection.class.getName());
 
+	private static boolean hml = true;
+
+	private static final String CONNECTION_STRING = "jdbc:mysql://54.167.113.168:3306/der_grupo_5";
+	private static final String USERNAME = "admin";
+	private static final String PASSWORD = "urubu100";
+
 	public static Connection ConBD() throws SQLException {
 		try {
-			return DriverManager.getConnection("jdbc:mysql://localhost/der_grupo_5", "client", "Client123$");
+			if (hml){
+				return DriverManager.getConnection(CONNECTION_STRING, USERNAME, PASSWORD);
+			} else {
+				return DriverManager.getConnection("jdbc:mysql://localhost/der_grupo_5", "client", "Client123$");
+			}
 		} catch (SQLException e) {
 			logger.log(Level.SEVERE, "Erro ao conectar ao banco de dados", e);
 			throw e;
