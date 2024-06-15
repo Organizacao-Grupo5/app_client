29c22fdf70b0cd2d859ba0a4d7126d0a6f2a4e68
thiagovieiras <thiago.santos@sptech.school>
2024-06-15 15:11:04 -0300

    atualizando o script do banco

--- app-client-api/src/main/java/util/database/der_grupo_5.sql
+++ app-client-api/src/main/java/util/database/der_grupo_5.sql
@@ -1,6 +1,6 @@
 -- SQLBook: Code
 -- DROP-Script
-DROP DATABASE der_grupo_5;
+DROP DATABASE IF EXISTS der_grupo_5;
 
 DROP USER 'client'@'%';
 FLUSH PRIVILEGES;
@@ -47,29 +47,29 @@ CREATE TABLE usuario (
     email VARCHAR(60) NOT NULL UNIQUE,
     senha VARCHAR(45) NOT NULL,
     cargo varchar(45),
-    fkEmpresa INT NOT NULL,
-        CONSTRAINT fkEmpresaUsuario FOREIGN KEY (fkEmpresa) REFERENCES empresa(idEmpresa)
-);
+    fkEmpresa INT NOT NULL, 
+        CONSTRAINT fkEmpresaUsuario FOREIGN KEY (fkEmpresa) REFERENCES empresa(idEmpresa) ON DELETE CASCADE
+); 
 
 CREATE TABLE contato (
     idContato INT AUTO_INCREMENT,
     telefone CHAR(12) NOT NULL,
     tipo VARCHAR(30) NOT NULL,
     fkUsuario INT NOT NULL,
-        CONSTRAINT fkUsuarioContato FOREIGN KEY (fkUsuario) REFERENCES usuario(idUsuario),
+        CONSTRAINT fkUsuarioContato FOREIGN KEY (fkUsuario) REFERENCES usuario(idUsuario) ON DELETE CASCADE,
     PRIMARY KEY (idContato, fkUsuario)
 );
 
 CREATE TABLE maquina (
     idMaquina INT PRIMARY KEY AUTO_INCREMENT,
-    numeroIdentificacao VARCHAR(25),
+    numeroIdentificacao VARCHAR(60),
     modelo VARCHAR(45),
     marca VARCHAR (45),
     mac VARCHAR(20),
     fkUsuario INT,
-        CONSTRAINT fkUsuarioMaquina FOREIGN KEY (fkUsuario) REFERENCES usuario(idUsuario),
+        CONSTRAINT fkUsuarioMaquina FOREIGN KEY (fkUsuario) REFERENCES usuario(idUsuario) ON DELETE CASCADE,
 	fkEmpresa INT NOT NULL,
-		CONSTRAINT fkEmpresaMaquina FOREIGN KEY (fkEmpresa) REFERENCES empresa(idEmpresa)
+		CONSTRAINT fkEmpresaMaquina FOREIGN KEY (fkEmpresa) REFERENCES empresa(idEmpresa) ON DELETE CASCADE
 );
 
 CREATE TABLE apps (
@@ -84,7 +84,7 @@ CREATE TABLE appAcessado (
     fkApp INT NOT NULL,
     CONSTRAINT fkAppMaquina FOREIGN KEY (fkApp) REFERENCES apps(idApp),
     fkMaquina INT NOT NULL,
-    CONSTRAINT fkMaquina FOREIGN KEY (fkMaquina) REFERENCES maquina(idMaquina),
+    CONSTRAINT fkMaquina FOREIGN KEY (fkMaquina) REFERENCES maquina(idMaquina) ON DELETE CASCADE,
     hora TIMESTAMP,
     PRIMARY KEY (fkApp, fkMaquina)
 );
@@ -104,14 +104,14 @@ CREATE TABLE ipv4(
     numeroIP VARCHAR(18),
     nomeLocal VARCHAR(45),
     fkMaquina INT NOT NULL,
-        CONSTRAINT fkMaquinaIPV4 FOREIGN KEY (fkMaquina) REFERENCES maquina(idMaquina)
+        CONSTRAINT fkMaquinaIPV4 FOREIGN KEY (fkMaquina) REFERENCES maquina(idMaquina) ON DELETE CASCADE
 );
 
 CREATE TABLE relRedeIpv4 (
     fkRede INT NOT NULL,
 		CONSTRAINT fkRedeRRI FOREIGN KEY (fkRede) REFERENCES rede(idRede),
 	fkIpv4 INT NOT NULL,
-		CONSTRAINT fkIpv4RRI FOREIGN KEY (fkIpv4) REFERENCES ipv4(idIpv4),
+		CONSTRAINT fkIpv4RRI FOREIGN KEY (fkIpv4) REFERENCES ipv4(idIpv4) ON DELETE CASCADE,
 	PRIMARY KEY (fkRede, fkIpv4),
 	dataConexao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
@@ -122,7 +122,7 @@ CREATE TABLE componente (
     modelo VARCHAR(200) NOT NULL,
     fabricante VARCHAR(200) NOT NULL,
     fkMaquina INT NOT NULL,
-        CONSTRAINT fkMaquinaComponente FOREIGN KEY (fkMaquina) REFERENCES maquina(idMaquina)
+        CONSTRAINT fkMaquinaComponente FOREIGN KEY (fkMaquina) REFERENCES maquina(idMaquina) ON DELETE CASCADE
 );
 
 CREATE TABLE configuracao (
@@ -130,16 +130,16 @@ CREATE TABLE configuracao (
     minimoParaSerMedio FLOAT NOT NULL DEFAULT 30,
 		CONSTRAINT ckMinimoParaSerMedio CHECK (minimoParaSerMedio < 100 AND minimoParaSerMedio > 0 AND minimoParaSerMedio < minimoParaSerRuim),
 	minimoParaSerRuim FLOAT NOT NULL DEFAULT 60,
-        CONSTRAINT ckMinimoParaSerRuim CHECK (minimoParaSerRuim < 100 AND minimoParaSerRuim > 0),
+        CONSTRAINT ckMinimoParaSerRuim CHECK (minimoParaSerRuim < 100 AND minimoParaSerRuim > 0),    
     dataModificacao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	fkComponente INT NOT NULL,
-		CONSTRAINT fkConfigComponente FOREIGN KEY (fkComponente) REFERENCES componente(idComponente)
+		CONSTRAINT fkConfigComponente FOREIGN KEY (fkComponente) REFERENCES componente(idComponente) ON DELETE CASCADE
 );
 
 CREATE TABLE alerta (
     idAlerta INT PRIMARY KEY AUTO_INCREMENT,
     mensagem VARCHAR(60) NOT NULL,
-	tipoAlerta VARCHAR(5) NOT NULL,
+	tipoAlerta VARCHAR(6) NOT NULL,
 		CONSTRAINT ckTipoAlerta CHECK (tipoAlerta IN ('BOM', 'MÉDIO', 'RUIM'))
 );
 
@@ -150,16 +150,17 @@ CREATE TABLE captura (
     dataCaptura DATETIME NOT NULL,
     dadoCapturaPercent FLOAT NOT NULL,
     fkComponente INT NOT NULL,
-        CONSTRAINT fkComponenteCaptura FOREIGN KEY (fkComponente) REFERENCES componente(idComponente)
+        CONSTRAINT fkComponenteCaptura FOREIGN KEY (fkComponente) REFERENCES componente(idComponente) ON DELETE CASCADE
 );
 
 CREATE TABLE registroAlerta (
     idRegistroAlertas INT PRIMARY KEY AUTO_INCREMENT,
     horario TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    visualizado BOOL DEFAULT FALSE,
     fkAlerta INT NOT NULL,
     constraint fkRegistroAlertasAlerta foreign key (fkAlerta) references alerta(idAlerta),
     fkCaptura INT NOT NULL,
-        CONSTRAINT fkRegistroAlertasCaptura FOREIGN KEY (fkCaptura) REFERENCES captura(idCaptura)
+        CONSTRAINT fkRegistroAlertasCaptura FOREIGN KEY (fkCaptura) REFERENCES captura(idCaptura) ON DELETE CASCADE
 );
 
 -- INSERT-Script
@@ -169,7 +170,7 @@ INSERT INTO plano VALUES
 	(NULL, 'Plano Corporativo', 'Foco: Grandes Empresas. Monitoramento de Hardware: Processador, RAM, disco, conexão USB, placa gráfica.');
 
 INSERT INTO empresa (nome, cnpj, fkPlano) VALUES
-('Gyllo Animations', '1574201230001', 1),
+('VisualOps', '1574201230001', 1),
 ('Empresa B', '98765432109876', 2),
 ('Empresa C', '56789012345678', 3);
 
@@ -178,38 +179,40 @@ INSERT INTO endereco (cep, logradouro, numero, fkEmpresa) VALUES
 ('98765-432', 'Rua B', '456', 2),
 ('56789-012', 'Rua C', '789', 3);
 
-INSERT INTO usuario (nome, email, senha, fkEmpresa) VALUES
-('Cláudio Araújo', 'l_claudio.araujo@hotmail.com', 'senha123', 1),
-('Maria Eduarda', 'l_maria.girote@outlook.com', 'senha123', 1),
-('Thiago Santos', 'l_thiago.santos@gmail.com', 'senha123', 1),
-('Guilherme Neves', 'l_guilherme.neves@hotmail.com', 'senha123', 1),
-('Diego Santos', 'l_diego.santos@outlook.com', 'senha123', 1),
-('Julia Campioto', 'l_julia.campioto@gmail.com', 'senha123', 1),
-('Cláudio Araújo', 'r_claudio.araujo@hotmail.com', 'senha123', 1),
-('Maria Eduarda', 'r_maria.girote@outlook.com', 'senha123', 1),
-('Thiago Santos', 'r_thiago.santos@gmail.com', 'senha123', 1),
-('Guilherme Neves', 'r_guilherme.neves@hotmail.com', 'senha123', 1),
-('Diego Santos', 'r_diego.santos@outlook.com', 'senha123', 1),
-('Julia Campioto', 'r_julia.campioto@gmail.com', 'senha123', 1);
+INSERT INTO usuario (nome, email, senha, cargo, fkEmpresa) VALUES
+('Cláudio Araújo', 'l_claudio.araujo@hotmail.com', 'senha123', 'Designer', 1),
+('Maria Eduarda', 'l_maria.girote@outlook.com', 'senha123', 'Designer', 1),
+('Thiago Santos', 'l_thiago.santos@gmail.com', 'senha123', 'Designer', 1),
+('Guilherme Neves', 'l_guilherme.neves@hotmail.com', 'senha123', 'Designer', 1),
+('Diego Santos', 'l_diego.santos@outlook.com', 'senha123', 'Designer', 1),
+('Julia Campioto', 'l_julia.campioto@gmail.com', 'senha123', 'Designer', 1),
+('Cláudio Araújo', 'r_claudio.araujo@hotmail.com', 'senha123', 'Designer', 1),
+('Maria Eduarda', 'r_maria.girote@outlook.com', 'senha123', 'Designer', 1),
+('Thiago Santos', 'r_thiago.santos@gmail.com', 'senha123', 'Designer', 1),
+('Guilherme Neves', 'r_guilherme.neves@hotmail.com', 'senha123', 'Designer', 1),
+('Diego Santos', 'r_diego.santos@outlook.com', 'senha123', 'Designer', 1),
+('Julia Campioto', 'r_julia.campioto@gmail.com', 'senha123', 'Designer', 1),
+('Maria Eduarda', 'l_maria.girote@gmail.com', 'senha123', 'Designer', 1),
+('VisualOps', 'visualops@outlook.com', 'senha123', 'Gerente', 1);
 
 SELECT * FROM maquina JOIN usuario on maquina.fkUsuario = usuario.idUsuario JOIN ipv4 ON ipv4.fkMaquina = maquina.idMaquina WHERE idUsuario = 3;
 
 SELECT * FROM maquina JOIN usuario on maquina.fkUsuario = usuario.idUsuario WHERE idUsuario = 3;
 
-INSERT INTO maquina (fkUsuario, fkEmpresa) VALUES
-(1, 1),
-(2, 1),
-(3, 1),
-(4, 1),
-(5, 1),
-(6, 1),
-(7, 1),
-(8, 1),
-(9, 1),
-(10, 1),
-(11, 1),
-(12, 1);
->>>>>>> 154d382636e315de128a35f55fe40c45b540dbd3
+INSERT INTO maquina (numeroIdentificacao, fkUsuario, fkEmpresa) VALUES 
+('929882', 1, 1),
+('529272', 2, 1),
+('317510', 3, 1),
+('492365', 4, 1),
+('180878', 5, 1),
+('466580', 6, 1),
+('380003', 7, 1),
+('573671', 8, 1),
+('289970', 9, 1),
+('328697', 10, 1),
+('692571', 11, 1),
+('650397', 12, 1),
+('325130', 13, 1);
 
 INSERT INTO alerta (mensagem, tipoAlerta) VALUES
 ('Componente em ótimo estado!', 'BOM'),
@@ -227,17 +230,19 @@ INSERT INTO alerta (mensagem, tipoAlerta) VALUES
 
 INSERT INTO ipv4(numeroIP, nomeLocal, fkMaquina) VALUES 
 ('192.168.15.28', 'Home', 1),
-    ('192.168.0.891', 'Home', 2),
-    ('192.168.15.5', 'Home', 3),
-    ('26.245.80.14', 'Home', 4),
-    ('10.0.0.109', 'Home', 5),
-    ('0.0.0.0', 'Home', 6),
-    ('0.0.0.0', 'Home', 7),
-    ('0.0.0.0', 'Home', 8),
-    ('172.31.61.112', 'Home', 9),
-    ('172.31.55.118', 'Home', 10),
-    ('0.0.0.0', 'Home', 11),
-    ('0.0.0.0', 'Home', 12);
+('192.168.0.891', 'Home', 2),
+('192.168.15.5', 'Home', 3),
+('26.245.80.14', 'Home', 4),
+('10.0.0.109', 'Home', 5),
+('0.0.0.0', 'Home', 6),
+('0.0.0.0', 'Home', 7),
+('0.0.0.0', 'Home', 8),
+('172.31.61.112', 'Home', 9),
+('172.31.55.118', 'Home', 10),
+('0.0.0.0', 'Home', 11),
+('0.0.0.0', 'Home', 12),
+('192.168.0.89', 'Home', 13);
+
 
 INSERT INTO rede (nomeRede, interfaceRede, sinalRede, transmissaoRede, bssidRede) 
 VALUES ('Net SPTech', 'Wi-Fi', 80, 2.4, '00:11:22:33:44:55');
@@ -399,8 +404,11 @@ SELECT COUNT(*) from maquina as m
 
 SELECT COLUMN_NAME, COLUMN_TYPE, COLUMN_DEFAULT, IS_NULLABLE, COLUMN_KEY, EXTRA, COLUMN_COMMENT FROM information_schema.columns WHERE TABLE_SCHEMA = 'der_grupo_5' AND TABLE_NAME = "alertas";
 
--- UPDATE-Script
-UPDATE ipv4 SET numeroIP = '192.168.15.5' WHERE idIpv4 =  1;
+SELECT * from captura join componente on idComponente = fkComponente;
 
-UPDATE ipv4 SET numeroIP = '10.18.34.239' WHERE fkMaquina = 4;
+SELECT componente, cap.unidadeMedida uni, COUNT(DAY(cap.dataCaptura)) dia FROM maquina mac
+	RIGHT JOIN componente comp ON idMaquina = fkMaquina
+		LEFT JOIN captura cap ON idComponente = fkComponente
+            WHERE fkEmpresa = 1
+		    	GROUP BY componente, uni;
 
